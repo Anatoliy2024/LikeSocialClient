dbb7bd50a3b9c45fbb260d2df80a45b10763d2dd
Anatoliy2024 <gredlend20@gmail.com>
2025-06-20 15:39:37 +0300

    little fix

--- src/utils/formatData.tsx
+++ src/utils/formatData.tsx
@@ -1,4 +1,4 @@
-export function formatData(data) {
+export function formatData(data: string) {
   const date = new Date(data)
   return `${String(date.getDate()).padStart(2, "0")}.${String(
     date.getMonth() + 1
