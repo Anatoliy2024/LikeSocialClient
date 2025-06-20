67c60bfc56c85079128b9cc0ee80a798147efcca
Anatoliy2024 <gredlend20@gmail.com>
2025-06-20 16:43:21 +0300

    change instance "/api/"

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL + "/api/" || "http://localhost:5000/api/",
   withCredentials: true, // важно: чтобы кука с refreshToken передавалась
 })
 
