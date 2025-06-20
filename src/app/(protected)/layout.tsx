0acfe140333b28a94f03fcf11df72b8a027d9391
Anatoliy2024 <gredlend20@gmail.com>
2025-06-20 21:26:22 +0300

    del function serverAuthAPI.check()

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,28 +1,28 @@
 // app/(protected)/layout.tsx
 
-import { serverAuthAPI } from "@/api/serverApi"
-import { redirect } from "next/navigation"
+// import { serverAuthAPI } from "@/api/serverApi"
+// import { redirect } from "next/navigation"
 
 export default async function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const res = await serverAuthAPI.check()
+  // const res = await serverAuthAPI.check()
 
-  console.log("res serverAuthAPI", res)
-  if (!res.ok) {
-    console.log("/register")
-    redirect("/register") // если не авторизован
-  }
+  // console.log("res serverAuthAPI", res)
+  // if (!res.ok) {
+  //   console.log("/register")
+  //   redirect("/register") // если не авторизован
+  // }
 
-  const user = await res.json()
+  // const user = await res.json()
 
-  if (!user.isVerified) {
-    console.log("/verify")
+  // if (!user.isVerified) {
+  //   console.log("/verify")
 
-    redirect("/verify")
-  }
+  //   redirect("/verify")
+  // }
 
   return <>{children}</>
 }
