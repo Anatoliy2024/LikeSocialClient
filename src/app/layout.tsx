375388253724ab0ff628dc89913e1e5967627c2a
Anatoliy2024 <gredlend20@gmail.com>
2025-06-27 16:33:59 +0300

    add reset css and fix scss module

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
+import "@/styles/reset.scss"
 // import Header from "@/components/header/Header"
 // import Navbar from "@/components/navbar/Navbar"
 // import Footer from "@/components/footer/Footer"
