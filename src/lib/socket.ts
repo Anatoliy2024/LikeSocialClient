49fd9306d9b02a120b24eae255670af54cb26915
Anatoliy2024 <gredlend20@gmail.com>
2025-07-19 13:51:39 +0300

    change socket io option

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -5,17 +5,23 @@ let socket: Socket | null = null
 
 export const getSocket = () => {
   if (!socket) {
-    socket = io(
+    const url =
       process.env.NEXT_PUBLIC_API_URL ||
-        (process.env.NODE_ENV === "development"
-          ? "http://localhost:5000"
-          : "https://likesocial.onrender.com"),
-      {
-        withCredentials: true,
-        autoConnect: false,
-        transports: ["websocket", "polling"],
-      }
-    )
+      (process.env.NODE_ENV === "development"
+        ? "http://localhost:5000"
+        : "https://likesocial.onrender.com")
+    console.log("url socket", url)
+    socket = io(url, {
+      withCredentials: true,
+      // autoConnect: false,
+      // transports: ["websocket", "polling"],
+
+      reconnection: true,
+      reconnectionAttempts: 50,
+      reconnectionDelay: 5000,
+      reconnectionDelayMax: 10000,
+      timeout: 10000,
+    })
   }
   return socket
 }
