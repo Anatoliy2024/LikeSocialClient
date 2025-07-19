a8651182d56ccf8fa6de00f49666edc168142d63
Anatoliy2024 <gredlend20@gmail.com>
2025-07-19 13:25:27 +0300

    fix url socket io

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -5,10 +5,16 @@ let socket: Socket | null = null
 
 export const getSocket = () => {
   if (!socket) {
-    socket = io("http://localhost:5000", {
-      withCredentials: true,
-      autoConnect: false,
-    })
+    socket = io(
+      process.env.NEXT_PUBLIC_API_URL ||
+        (process.env.NODE_ENV === "development"
+          ? "http://localhost:5000/"
+          : "https://likesocial.onrender.com/"),
+      {
+        withCredentials: true,
+        autoConnect: false,
+      }
+    )
   }
   return socket
 }
