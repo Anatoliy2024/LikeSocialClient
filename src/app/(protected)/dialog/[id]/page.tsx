2ac31568a46dfa8a3d5e4182f681a37c7eeba83d
Anatoliy2024 <gredlend20@gmail.com>
2026-02-21 23:18:48 +0300

    comment dialog page

--- src/app/(protected)/dialog/[id]/page.tsx
+++ src/app/(protected)/dialog/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { MessageBlock } from "@/components/MessageBlock/MessageBlock"
-import { Suspense } from "react"
+// import { MessageBlock } from "@/components/MessageBlock/MessageBlock"
+// import { Suspense } from "react"
 
-export default function Dialog() {
-  return (
-    <Suspense>
-      <MessageBlock />
-    </Suspense>
-  )
-}
+// export default function Dialog() {
+//   return (
+//     <Suspense>
+//       <MessageBlock />
+//     </Suspense>
+//   )
+// }
--- src/app/(protected)/dialogs/page.tsx
+++ src/app/(protected)/dialogs/page.tsx
@@ -1,112 +1,112 @@
-"use client"
-import { useEffect, useState } from "react"
-import style from "./Dialogs.module.scss"
-import { useAppDispatch, useAppSelector } from "@/store/hooks"
-import { getUserDialogsThunk } from "@/store/thunks/dialogsThunk"
-import { RootState } from "@/store/store"
-// import Image from "next/image"
-// import { formatData } from "@/utils/formatData"
-import { useRouter } from "next/navigation"
-import { CloudinaryImage } from "@/components/CloudinaryImage/CloudinaryImage"
-import { formatMessageTime } from "@/utils/formatMessageTime"
-import SpinnerWindow from "@/components/ui/spinner/SpinnerWindow"
-import ButtonMenu from "@/components/ui/button/Button"
-import { ModalAddGroup } from "@/components/ModalAddGroup/ModalAddGroup"
+// "use client"
+// import { useEffect, useState } from "react"
+// import style from "./Dialogs.module.scss"
+// import { useAppDispatch, useAppSelector } from "@/store/hooks"
+// import { getUserDialogsThunk } from "@/store/thunks/dialogsThunk"
+// import { RootState } from "@/store/store"
+// // import Image from "next/image"
+// // import { formatData } from "@/utils/formatData"
+// import { useRouter } from "next/navigation"
+// import { CloudinaryImage } from "@/components/CloudinaryImage/CloudinaryImage"
+// import { formatMessageTime } from "@/utils/formatMessageTime"
+// import SpinnerWindow from "@/components/ui/spinner/SpinnerWindow"
+// import ButtonMenu from "@/components/ui/button/Button"
+// import { ModalAddGroup } from "@/components/ModalAddGroup/ModalAddGroup"
 
-export default function Dialogs() {
-  const [showAddGroup, setShowAddGroup] = useState(false)
-  const dispatch = useAppDispatch()
-  const dialogs = useAppSelector((state: RootState) => state.dialogs.dialogs)
-  const loading = useAppSelector((state: RootState) => state.dialogs.loading)
-  const userId = useAppSelector((state: RootState) => state.auth.userId)
-  const usersOnline = useAppSelector((state: RootState) => state.onlineStatus)
+// export default function Dialogs() {
+//   const [showAddGroup, setShowAddGroup] = useState(false)
+//   const dispatch = useAppDispatch()
+//   const dialogs = useAppSelector((state: RootState) => state.dialogs.dialogs)
+//   const loading = useAppSelector((state: RootState) => state.dialogs.loading)
+//   const userId = useAppSelector((state: RootState) => state.auth.userId)
+//   const usersOnline = useAppSelector((state: RootState) => state.onlineStatus)
 
-  const router = useRouter()
-  const handleLinkDialog = (dialogId: string) => {
-    router.push(`/dialog/${dialogId}`)
-  }
+//   const router = useRouter()
+//   const handleLinkDialog = (dialogId: string) => {
+//     router.push(`/dialog/${dialogId}`)
+//   }
 
-  useEffect(() => {
-    dispatch(getUserDialogsThunk())
-  }, [dispatch])
-  // console.log("dialogs", dialogs)
+//   useEffect(() => {
+//     dispatch(getUserDialogsThunk())
+//   }, [dispatch])
+//   // console.log("dialogs", dialogs)
 
-  const closeModalAddGroup = () => {
-    setShowAddGroup(false)
-  }
-  console.log("dialog")
+//   const closeModalAddGroup = () => {
+//     setShowAddGroup(false)
+//   }
+//   console.log("dialog")
 
-  return (
-    <>
-      {loading && <SpinnerWindow />}
-      <div className={style.containerDialogs}>
-        {showAddGroup && (
-          <ModalAddGroup closeModalAddGroup={closeModalAddGroup} />
-        )}
-        <div className={style.buttonBlock}>
-          <ButtonMenu
-            onClick={() => {
-              setShowAddGroup(true)
-              // dispatch(createRoomThunk(name))
-            }}
-          >
-            Создать группу
-          </ButtonMenu>
-        </div>
-        {/* <h2>Dialogs</h2> */}
-        {dialogs.map.length > 0 ? (
-          <ul className={style.dialogLists}>
-            {dialogs.map((dialog) => {
-              const member = dialog.members.filter(
-                (member) => member._id !== userId
-              )[0]
+//   return (
+//     <>
+//       {loading && <SpinnerWindow />}
+//       <div className={style.containerDialogs}>
+//         {showAddGroup && (
+//           <ModalAddGroup closeModalAddGroup={closeModalAddGroup} />
+//         )}
+//         <div className={style.buttonBlock}>
+//           <ButtonMenu
+//             onClick={() => {
+//               setShowAddGroup(true)
+//               // dispatch(createRoomThunk(name))
+//             }}
+//           >
+//             Создать группу
+//           </ButtonMenu>
+//         </div>
+//         {/* <h2>Dialogs</h2> */}
+//         {dialogs.map.length > 0 ? (
+//           <ul className={style.dialogLists}>
+//             {dialogs.map((dialog) => {
+//               const member = dialog.members.filter(
+//                 (member) => member._id !== userId
+//               )[0]
 
-              return (
-                <li
-                  key={dialog._id}
-                  className={style.dialogList}
-                  onClick={() => {
-                    handleLinkDialog(dialog._id)
-                  }}
-                >
-                  {/* <div className={style.mainInfoUserBlock}> */}
-                  <div className={style.containerImageDialogs}>
-                    <div className={style.blockImageDialogs}>
-                      <CloudinaryImage
-                        src={member.avatar}
-                        width={200}
-                        height={200}
-                        alt="userAvatar"
-                      />
-                    </div>
-                    {usersOnline[member._id]?.isOnline && (
-                      <div className={style.onlineBlockDialogs}></div>
-                    )}
-                  </div>
-                  <div className={style.dialogInfo}>
-                    <div className={style.userName}>{member.username}</div>
+//               return (
+//                 <li
+//                   key={dialog._id}
+//                   className={style.dialogList}
+//                   onClick={() => {
+//                     handleLinkDialog(dialog._id)
+//                   }}
+//                 >
+//                   {/* <div className={style.mainInfoUserBlock}> */}
+//                   <div className={style.containerImageDialogs}>
+//                     <div className={style.blockImageDialogs}>
+//                       <CloudinaryImage
+//                         src={member.avatar}
+//                         width={200}
+//                         height={200}
+//                         alt="userAvatar"
+//                       />
+//                     </div>
+//                     {usersOnline[member._id]?.isOnline && (
+//                       <div className={style.onlineBlockDialogs}></div>
+//                     )}
+//                   </div>
+//                   <div className={style.dialogInfo}>
+//                     <div className={style.userName}>{member.username}</div>
 
-                    {dialog?.lastMessageId && (
-                      <div className={style.dialogLastMessage}>
-                        <div className={style.dialogLastMessageContent}>
-                          <div>{dialog.lastMessageId.senderId.username}:</div>
-                          <div>{dialog.lastMessageId.text}</div>
-                        </div>
+//                     {dialog?.lastMessageId && (
+//                       <div className={style.dialogLastMessage}>
+//                         <div className={style.dialogLastMessageContent}>
+//                           <div>{dialog.lastMessageId.senderId.username}:</div>
+//                           <div>{dialog.lastMessageId.text}</div>
+//                         </div>
 
-                        <div className={style.timeBlock}>
-                          {formatMessageTime(dialog.lastMessageId.createdAt)}
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                </li>
-              )
-            })}
-          </ul>
-        ) : (
-          <div>Диалогов пока нет</div>
-        )}
-      </div>
-    </>
-  )
-}
+//                         <div className={style.timeBlock}>
+//                           {formatMessageTime(dialog.lastMessageId.createdAt)}
+//                         </div>
+//                       </div>
+//                     )}
+//                   </div>
+//                 </li>
+//               )
+//             })}
+//           </ul>
+//         ) : (
+//           <div>Диалогов пока нет</div>
+//         )}
+//       </div>
+//     </>
+//   )
+// }
