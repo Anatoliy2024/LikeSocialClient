864cb9c9cd6718dbf32fb4c4405b1ac41d90fd03
Anatoliy2024 <gredlend20@gmail.com>
2026-03-04 16:59:18 +0300

    add more pack of stickers

--- src/components/StickersBlock/StickersBlock.tsx
+++ src/components/StickersBlock/StickersBlock.tsx
@@ -2,7 +2,7 @@
 import { STICKERS, StickersTypeItem } from "@/constants/stickers"
 import style from "./StickersBlock.module.scss"
 import Image from "next/image"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export function StickersBlock({
   onClose,
@@ -12,6 +12,7 @@ export function StickersBlock({
   handleSendSticker: (stickerId: string) => void
 }) {
   const keys = Object.keys(STICKERS)
+  const [stickerPack, setStickerPack] = useState(keys[0])
 
   useEffect(() => {
     // при монтировании — запрещаем прокрутку
@@ -27,15 +28,32 @@ export function StickersBlock({
     <>
       <div className={style.background} onClick={onClose}></div>
       <div className={style.stickersBlock}>
-        {keys.map((stickerKey) => {
-          return (
-            <StickerPack
-              stickers={STICKERS[stickerKey]}
+        <ul className={style.stickersBlock__listPacket}>
+          {keys.map((stickerKey, i) => (
+            <li
               key={stickerKey}
-              handleSendSticker={handleSendSticker}
-            />
-          )
-        })}
+              onClick={() => setStickerPack(stickerKey)}
+              className={
+                stickerKey === stickerPack
+                  ? style.stickersBlock__listPacketActive
+                  : ""
+              }
+            >
+              <Image
+                src={STICKERS[keys[i]][0].image}
+                width={70}
+                height={70}
+                alt={stickerKey}
+              />
+            </li>
+          ))}
+        </ul>
+        <div className={style.stickersBlock__dividingLine}></div>
+        <StickerPack
+          stickers={STICKERS[stickerPack]}
+          // key={stickerKey}
+          handleSendSticker={handleSendSticker}
+        />
       </div>
     </>
   )
@@ -48,14 +66,19 @@ const StickerPack = ({
   stickers: StickersTypeItem[]
   handleSendSticker: (stickerId: string) => void
 }) => {
-  return stickers.map((sticker) => (
-    <Image
-      key={sticker.id}
-      src={sticker.image}
-      width={120}
-      height={120}
-      alt={sticker.id}
-      onClick={() => handleSendSticker(sticker.id)}
-    />
-  ))
+  return (
+    <ul className={style.stickersPackList}>
+      {stickers.map((sticker) => (
+        <li key={sticker.id}>
+          <Image
+            src={sticker.image}
+            width={120}
+            height={120}
+            alt={sticker.id}
+            onClick={() => handleSendSticker(sticker.id)}
+          />
+        </li>
+      ))}
+    </ul>
+  )
 }
