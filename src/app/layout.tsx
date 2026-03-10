f789007eda5a4a0d210a4d97e8ea1b1be517f3d3
Anatoliy2024 <gredlend20@gmail.com>
2026-03-10 03:43:51 +0300

    dell <link rel="manifest" href="/manifest.json" />

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
+
         <meta name="theme-color" content="#000000" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
