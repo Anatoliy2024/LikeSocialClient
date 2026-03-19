ad7d062b3d1e5d528279a5e57a0a96dc5fd8845d
Anatoliy2024 <gredlend20@gmail.com>
2026-03-20 00:48:45 +0300

    dell comment

--- src/hooks/useCall.ts
+++ src/hooks/useCall.ts
@@ -41,21 +41,6 @@ type Maybe<T> = T | null
 const MAX_RECONNECT_ATTEMPTS = 2
 const RECONNECT_DELAY_MS = 2000
 
-// const ICE_SERVERS = [
-//   { urls: "stun:stun.l.google.com:19302" },
-//   { urls: "stun:stun1.l.google.com:19302" },
-//   { urls: "stun:stun2.l.google.com:19302" },
-//   { urls: "stun:stun3.l.google.com:19302" },
-//   { urls: "stun:stun4.l.google.com:19302" },
-
-//   // Cloudflare — бесплатный, без лимитов
-//   { urls: "stun:stun.cloudflare.com:3478" },
-
-//   // Резервные от других провайдеров
-//   { urls: "stun:stun.stunprotocol.org:3478" },
-//   { urls: "stun:stun.voip.blackberry.com:3478" },
-// ]
-
 export const useCall = (userId: string | null) => {
   const [loadingConnect, setLoadingConnect] = useState(false)
   const dispatch = useAppDispatch()
